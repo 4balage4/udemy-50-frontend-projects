dec806b078fed55691269afa976f8f6c13693be0
balazs burian <bur.balazs@protonmail.com>
2024-08-22 09:56:18 +0200

    done

---
+++ faq-collapse/script.js
@@ -0,0 +1,10 @@
+
+const buttons = document.querySelectorAll('.faq-toggle')
+
+
+
+buttons.forEach(element => {
+  element.addEventListener('click', () => {
+    element.parentNode.classList.toggle('active')
+  } )
+})
